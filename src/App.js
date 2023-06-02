d24b13112ac25857677ccedf669a9b41a7845e8c
ashish-singh96 <ashishbhadauriya5344@gmail.com>
2023-06-02 23:50:20 +0530

    DataJson

--- src/App.js
+++ src/App.js
@@ -6,32 +6,29 @@ import Header from './component/Navbar/Record Keeping/Header';
 import Text from './component/Navbar/Record Keeping/Text';
 import Data from './component/Product API/Data';
 import UsingFetch from './component/Product API/UsingFetch';
+import { imageListClasses } from '@mui/material';
 
 function App() {
   return (
-    <div className='App
-    '>
-      {/* <Navbar/> */}
-      {/* <div className='main'>{
-        Home.map((element) => {
-          return (
-            <HomeUi
-              img={element.img}
-              title={element.title}
-              price={element.price}
-            />
-          )
-        })
-      }
-      </div> */}
+    <div>
+     {/* <Navbar/>
+     <div className='main'>{
+     Home.map((element)=>{
+      return(
+        <HomeUi
+          img={element.img}
+          price={element.price}
+          title={element.title}
+          about={element.about}
+          Gupta={element.Gupta}
+        />
+      )
+     })
+     }
 
-      {/* <Header />
-      <Text /> */}
+     </div> */}
 
-      {/* <Data/> */}
-      <UsingFetch/>
-
-    </div>
+    </div> 
   );
 }
 
--- src/component/HomeUi.js
+++ src/component/HomeUi.js
@@ -2,10 +2,14 @@ import React from 'react'
 import './HomeUi.css';
 const HomeUi = (props) => {
   return (
-    <div className='home'>
-        <img src={props.img} alt=''/>
-        <h3>Price {props.price}</h3>
-        <p>{props.title}</p>
+    <div>
+      <div className='home'>
+       <img src={props.img}></img>
+       <h3>{props.price}</h3>
+       <h3>{props.title}</h3>
+       <p>{props.about}</p>
+       <h4>{props.Gupta}</h4>
+      </div>
     </div>
   )
 }
