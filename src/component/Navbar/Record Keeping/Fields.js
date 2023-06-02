fd9c63f92648641a9a7b9f08ff2a78346cbf86ca
ashish-singh96 <ashishbhadauriya5344@gmail.com>
2023-06-02 08:56:25 +0530

    Done

--- src/component/Navbar/Record Keeping/Fields.js
+++ src/component/Navbar/Record Keeping/Fields.js
@@ -4,17 +4,20 @@ import Stack from '@mui/material/Stack';
 import DeleteIcon from '@mui/icons-material/Delete';
 import './Text.css'
 const Fields = ({name, email, index}) => {
+    const remove = () =>{
+        
+    }
     return (
-        <Stack direction="row" spacing={2}>
+        // <Stack direction="row" spacing={2}>
+       
             <div className='data_val'>
                 <h4>{name}</h4>
                 <h4>{email}</h4>
-                <Button variant="contained" color="error"><DeleteIcon/></Button>
-            </div>
-           
-
-
-        </Stack>
+                <Stack>
+                <Button onClick={()=>remove(index)} variant="contained" color="error"><DeleteIcon/></Button>
+                </Stack>
+            </div> 
+        // </Stack>
 
     )
 }
--- src/component/Navbar/Record Keeping/Text.js
+++ src/component/Navbar/Record Keeping/Text.js
@@ -6,44 +6,56 @@ import Stack from '@mui/material/Stack';
 import AddIcon from '@mui/icons-material/Add';
 import Fields from './Fields';
 import { keys } from '@mui/system';
+import DeleteIcon from '@mui/icons-material/Delete';
 const Text = () => {
-    const[name,setName]=useState("");
-    const[email,setEmail]=useState("");
-    const[data,setData]=useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [data, setData] = useState([]);
 
-    const addData = ()=>{
-        setData([...data,{name,email}])
-        setName("")
-        setEmail("")
-    }
+  const addData = () => {
+    setData([...data, { name, email }])
+    setName("")
+    setEmail("")
+  }
+
+
+  const removeItem = (index) => {
+    let arr = data;
+    arr.splice(index, 1);
+    setData([...arr]);
+  }
 
   return (
     <>
-    <div>
+      <div>
         <Stack className='text' spacing={2} direction="row">
-        <TextField value={name} onChange={(e)=>setName(e.target.value)} id="outlined-basic" label="Name" variant="outlined" />
-        <TextField value={email} onChange={(e)=>setEmail(e.target.value)} id="outlined-basic" label="E-mail" variant="outlined" />
-        <Button onClick={addData} variant="contained" color='success'><AddIcon/></Button>
+          <TextField value={name} onChange={(e) => setName(e.target.value)} id="outlined-basic" label="Name" variant="outlined" />
+          <TextField value={email} onChange={(e) => setEmail(e.target.value)} id="outlined-basic" label="E-mail" variant="outlined" />
+          <Button onClick={addData} variant="contained" color='success'><AddIcon /></Button>
         </Stack>
-    </div>
-    
-    <div className='data'>
-    <div className='data_val'>
-    <h4>Name</h4>
-    <h4>Email</h4>
-    <h4>Remove</h4>
-    </div>
-    {
-      data.map((element,index)=>{
-        return(
-          <Fields
-             key={index} name={element.name} email={element.email} index={index}
-          />
-        )
-      })
-    }
+      </div>
 
-    </div>
+      <div className='data'>
+        <div className='data_val'>
+          <h4>Name</h4>
+          <h4>Email</h4>
+          <h4>Remove</h4>
+        </div>
+        {
+          data.map((element, index) => {
+            return (
+              <div key={index} className='data_val'>
+                <h4>{element.name}</h4>
+                <h4>{element.email}</h4>
+                <Stack>
+                  <Button onClick={() => removeItem(index)} variant="contained" color="error"><DeleteIcon /></Button>
+                </Stack>
+              </div>
+            )
+          })
+        }
+
+      </div>
     </>
   )
 }
